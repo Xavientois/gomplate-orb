731a0da57851cfde0bc071cd6f3d2fe8fb9828cd
Xavientois <joshua.rampersad@hotmail.com>
2023-02-26 15:30:02 -0500

    sudo cp

--- src/scripts/install-gomplate.sh
+++ src/scripts/install-gomplate.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-curl -o /usr/local/bin/gomplate -sSL "https://github.com/hairyhenderson/gomplate/releases/download/${VERSION:?}/gomplate_${OS:?}-${ARCH:?}"
-chmod 755 /usr/local/bin/gomplate
+curl -o ./gomplate -sSL "https://github.com/hairyhenderson/gomplate/releases/download/${VERSION:?}/gomplate_${OS:?}-${ARCH:?}"
+chmod 755 ./gomplate
+sudo cp ./gomplate /usr/local/bin/gomplate
 gomplate --help
