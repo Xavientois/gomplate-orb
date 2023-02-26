ef310279de041e429e6431e02207d94bb82d3905
Xavientois <joshua.rampersad@hotmail.com>
2023-02-26 13:31:07 -0500

    add missing files

---
+++ src/scripts/render-template.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This script generates arguments for and runs the gomplate command to render the template
+
+ARGS=()
+ARGS+=("--config" "${GOMPLATE_CONFIG?}")
+ARGS+=("--file" "${TEMPLATE_FILE?}")
+ARGS+=("--out" "${OUTPUT_FILE?}")
+
+for DATASOURCE in ${DATASOURCES?}; do
+  ARGS+=("--datasource" "${DATASOURCE}")
+done
+
+for CONTEXT in ${CONTEXTS?}; do
+  ARGS+=("--context" "${CONTEXT}")
+done
+
+echo "Running \"gomplate ${ARGS[@]}\""
+gomplate ${ARGS[@]}
